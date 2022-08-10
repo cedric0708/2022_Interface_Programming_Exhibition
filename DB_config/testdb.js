01782c4b7bec3685cb5d1449505ec4beb78a5d27
Gwanyeop Kim <81212131+cedric0708@users.noreply.github.com>
2022-08-10 14:42:21 +0900

    Create testdb.js

---
+++ DB_config/testdb.js
@@ -0,0 +1,8 @@
+module.exports = {
+    host     : 'localhost',
+    user     : 'root',
+    password : '1029384756a@',
+    database : 'test',
+    multipleStatements: true,
+    enableKeepAlive: true
+};
